b2fce19e5ec6275107497a98ee98cc448b4a3d56
Masayuki Keida <masayuki.keida+github@gmail.com>
2024-10-01 15:07:44 +0900

    Step 25

--- kilo.c
+++ kilo.c
@@ -61,8 +61,20 @@ editorReadKey(void) {
 /*** output ***/
 
 void
+editorDrawRows(void) {
+	int y;
+	for (y = 0; y < 24; y++) {
+		write(STDOUT_FILENO, "~\r\n", 3);
+	}
+}
+
+void
 editorRefreshScreen(void) {
 	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
+	editorDrawRows();
+
 	write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
