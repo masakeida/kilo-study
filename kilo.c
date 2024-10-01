a7c8731c76425aba185a489ef4c3bd3b97cca798
Masayuki Keida <masayuki.keida+github@gmail.com>
2024-10-01 09:06:08 +0900

    Step 13

--- kilo.c
+++ kilo.c
@@ -18,6 +18,7 @@ enableRawMode(void) {
 
 	struct termios raw = orig_termios;
 	raw.c_iflag &= ~(ICRNL | IXON);
+	raw.c_oflag &= ~(OPOST);
 	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
