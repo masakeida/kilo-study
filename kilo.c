e0e0857fb5776b79815f5ff8f7292bc5268df134
Masayuki Keida <masayuki.keida+github@gmail.com>
2024-10-01 15:15:36 +0900

    Step 29

--- kilo.c
+++ kilo.c
@@ -83,7 +83,7 @@ getWindowSize(int *rows, int *cols) {
 void
 editorDrawRows(void) {
 	int y;
-	for (y = 0; y < 24; y++) {
+	for (y = 0; y < E.screenrows; y++) {
 		write(STDOUT_FILENO, "~\r\n", 3);
 	}
 }
