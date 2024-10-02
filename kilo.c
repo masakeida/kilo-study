34daded805abc4eb4d4b78748c17fd124638d6ec
Masayuki Keida <masayuki.keida+github@gmail.com>
2024-10-02 12:54:44 +0900

    Step 73

--- kilo.c
+++ kilo.c
@@ -243,6 +243,12 @@ editorScroll(void) {
 	if (E.cy >= E.rowoff + E.screenrows) {
 		E.rowoff = E.cy - E.screenrows + 1;
 	}
+	if (E.cx < E.coloff) {
+		E.coloff = E.cx;
+	}
+	if (E.cx >= E.coloff + E.screencols) {
+		E.coloff = E.cx - E.screencols + 1;
+	}
 }
 
 void
