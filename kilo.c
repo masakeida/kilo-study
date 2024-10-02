90271ee126d22d0b3156d73f75635b6b3f866ad7
Masayuki Keida <masayuki.keida+github@gmail.com>
2024-10-02 13:33:20 +0900

    Step 94

--- kilo.c
+++ kilo.c
@@ -51,6 +51,7 @@ struct editorConfig {
 	int screencols;
 	int numrows;
 	erow *row;
+	char *filename;
 	struct termios orig_termios;
 };
 
@@ -237,6 +238,9 @@ editorAppendRow(char *s, size_t len) {
 
 void
 editorOpen(char *filename) {
+	free(E.filename);
+	E.filename = strdup(filename);
+
 	FILE *fp = fopen(filename, "r");
 	if (!fp) die("fopen");
 
@@ -467,6 +471,7 @@ initEditor(void) {
 	E.coloff = 0;
 	E.numrows = 0;
 	E.row = NULL;
+	E.filename = NULL;
 
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 	E.screenrows -= 1;
