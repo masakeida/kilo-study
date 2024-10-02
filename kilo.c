5f0324863173172c47b6a6efa2d4b268e75ab724
Masayuki Keida <masayuki.keida+github@gmail.com>
2024-10-03 08:45:07 +0900

    Step 121

--- kilo.c
+++ kilo.c
@@ -306,11 +306,17 @@ editorInsertChar(int c) {
 void
 editorDelChar(void) {
 	if (E.cy == E.numrows) return;
+	if (E.cx == 0 && E.cy == 0) return;
 
 	erow *row = &E.row[E.cy];
 	if (E.cx > 0) {
 		editorRowDelChar(row, E.cx - 1);
 		E.cx--;
+	} else {
+		E.cx = E.row[E.cy - 1].size;
+		editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
+		editorDelRow(E.cy);
+		E.cy--;
 	}
 }
 
