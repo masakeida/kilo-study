169307659eaf9291c8cb3cfc04b6b26c8206a0e3
Masayuki Keida <masayuki.keida+github@gmail.com>
2024-10-02 20:33:17 +0900

    Step 106

--- kilo.c
+++ kilo.c
@@ -6,6 +6,7 @@
 
 #include <ctype.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
@@ -303,6 +304,20 @@ editorOpen(char *filename) {
 	}
 	free(line);
 	fclose(fp);
+}
+
+void
+editorSave(void) {
+	if (E.filename == NULL) return;
+
+	int len;
+	char *buf = editorRowsToString(&len);
+
+	int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+	ftruncate(fd, len);
+	write(fd, buf, len);
+	close(fd);
+	free(buf);
 }
 
 /*** append buffer ***/
