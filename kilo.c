950081dda9a2828781dbb8c445c6077c70fbc5ef
Masayuki Keida <masayuki.keida+github@gmail.com>
2024-10-02 13:19:04 +0900

    Step 86

--- kilo.c
+++ kilo.c
@@ -270,17 +270,19 @@ abFree(struct abuf *ab) {
 
 void
 editorScroll(void) {
+	E.rx = E.cx;
+
 	if (E.cy < E.rowoff) {
 		E.rowoff = E.cy;
 	}
 	if (E.cy >= E.rowoff + E.screenrows) {
 		E.rowoff = E.cy - E.screenrows + 1;
 	}
-	if (E.cx < E.coloff) {
-		E.coloff = E.cx;
+	if (E.rx < E.coloff) {
+		E.coloff = E.rx;
 	}
-	if (E.cx >= E.coloff + E.screencols) {
-		E.coloff = E.cx - E.screencols + 1;
+	if (E.rx >= E.coloff + E.screencols) {
+		E.coloff = E.rx - E.screencols + 1;
 	}
 }
 
