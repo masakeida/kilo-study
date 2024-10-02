39adcbea9a6786b8a228443d78ec3ab739388f6a
Masayuki Keida <masayuki.keida+github@gmail.com>
2024-10-03 06:34:15 +0900

    Step 115

--- kilo.c
+++ kilo.c
@@ -21,6 +21,7 @@
 
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 8
+#define KILO_QUIT_TIMES 3
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -528,6 +529,8 @@ editorMoveCursor(int key) {
 
 void
 editorProcessKeypress(void) {
+	static int quit_times = KILO_QUIT_TIMES;
+
 	int c = editorReadKey();
 
 	switch (c) {
@@ -536,6 +539,12 @@ editorProcessKeypress(void) {
 		break;
 
 	case CTRL_KEY('q'):
+		if (E.dirty && quit_times > 0) {
+			editorSetStatusMessage("WARNING!!! File has unsaved changes. "
+					       "Press Ctrl-Q %d more times to quit.", quit_times);
+			quit_times--;
+			return;
+		}
 		write(STDOUT_FILENO, "\x1b[2J", 4);
 		write(STDOUT_FILENO, "\x1b[H", 3);
 		exit(0);
@@ -593,6 +602,8 @@ editorProcessKeypress(void) {
 		editorInsertChar(c);
 		break;
 	}
+
+	quit_times = KILO_QUIT_TIMES;
 }
 
 /*** init ***/
