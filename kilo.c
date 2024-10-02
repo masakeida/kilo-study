d1136033b15adfce8aba2c72395cd476e8a171f7
Masayuki Keida <masayuki.keida+github@gmail.com>
2024-10-02 13:01:33 +0900

    Step 77

--- kilo.c
+++ kilo.c
@@ -335,6 +335,12 @@ editorMoveCursor(int key) {
 		}
 		break;
 	}
+
+	row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+	int rowlen = row ? row->size : 0;
+	if (E.cx > rowlen) {
+		E.cx = rowlen;
+	}
 }
 
 void
