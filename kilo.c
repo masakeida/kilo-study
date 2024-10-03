9fd8ad525fe8e01a647e48bb1eaed979da41817b
Masayuki Keida <masayuki.keida+github@gmail.com>
2024-10-03 15:12:42 +0900

    Step 134

--- kilo.c
+++ kilo.c
@@ -712,6 +712,10 @@ editorProcessKeypress(void) {
 		}
 		break;
 
+	case CTRL_KEY('f'):
+		editorFind();
+		break;
+
 	case BACKSPACE:
 	case CTRL_KEY('h'):
 	case DEL_KEY:
@@ -783,7 +787,8 @@ main(int argc, char *argv[]) {
 		editorOpen(argv[1]);
 	}
 
-	editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
+	editorSetStatusMessage(
+		"HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
 
 	while (1) {
 		editorRefreshScreen();
