637c145e84377ba04f65a3bb43c10ad0fd8c47eb
Masayuki Keida <masayuki.keida+github@gmail.com>
2024-10-03 09:27:40 +0900

    Step 122: Doesn't compile

--- kilo.c
+++ kilo.c
@@ -230,10 +230,12 @@ editorUpdateRow(erow *row) {
 }
 
 void
-editorAppendRow(char *s, size_t len) {
-	E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+editorInsertRow(int at, char *s, size_t len) {
+	if (at < 0 || at > E.numrows) return;
 
-	int at = E.numrows;
+	E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+	memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+
 	E.row[at].size = len;
 	E.row[at].chars = malloc(len + 1);
 	memcpy(E.row[at].chars, s, len);
