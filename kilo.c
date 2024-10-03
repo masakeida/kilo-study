9fb5df1a82efa84a63d8ed3fe06af71fc03e0cc6
Masayuki Keida <masayuki.keida+github@gmail.com>
2024-10-03 15:58:11 +0900

    Step 150

--- kilo.c
+++ kilo.c
@@ -40,7 +40,8 @@ enum editorKey {
 
 enum editorHighlight {
 	HL_NORMAL = 0,
-	HL_NUMBER
+	HL_NUMBER,
+	HL_MATCH
 };
 
 /*** data ***/
@@ -218,6 +219,7 @@ int
 editorSyntaxToColor(int hl) {
 	switch (hl) {
 	case HL_NUMBER: return 31;
+	case HL_MATCH: return 34;
 	default: return 37;
 	}
 }
