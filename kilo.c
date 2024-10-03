0cd824777003dacf687b134542334aea32e51e80
Masayuki Keida <masayuki.keida+github@gmail.com>
2024-10-03 15:49:06 +0900

    Step 147

--- kilo.c
+++ kilo.c
@@ -214,6 +214,14 @@ editorUpdateSyntax(erow *row) {
 	}
 }
 
+int
+editorSyntaxToColor(int hl) {
+	switch (hl) {
+	case HL_NUMBER: return 31;
+	default: return 37;
+	}
+}
+
 /*** row operations ***/
 
 int
